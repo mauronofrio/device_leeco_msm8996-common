b8711bd50fca0083338dab9117f67b6f166b6a0c
gabr0 <gabronogue@gmail.com>
2018-04-05 20:36:10 +0200

    msm8996: Add kcal and fix related denials

--- msm8996.mk
+++ msm8996.mk
@@ -199,6 +199,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
+# KCAL
+PRODUCT_PACKAGES += \
+    kcal
+
 # Doze mode
 PRODUCT_PACKAGES += \
     Doze
